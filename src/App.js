6b9db7d9a3b62f6732872bc61fa1f73fd919d5e5
asedghi <asedghi@sfu.ca>
2017-08-16 17:37:46 -0700

    trying to align the colums to display projects with symmetry

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-//import {ShowHide}  from './components/projects.js'
+import logo from './assets/logo.svg';
+import bookmark from './assets/bookmark.png';
+import calc from './assets/calc.png';
 import './App.css';
 
 const image = "https://avatars0.githubusercontent.com/u/15239463?v=4&s=460"
@@ -47,57 +48,57 @@ class App extends Component {
         <p className="App-intro">
             Below you can find a list of all the projects that I have worked on. 
         </p>
-        <div className="Projects"> 
+        <div className="Projects" id="wrapper"> 
           <ul>
-              <li> WebApp To Do List </li>
-              <img src={logo} alt="1st" onClick={() => this.onClick(0)}/>
-              {
-                  this.state.children[0].childVisible
-                  ? <p> This app was build on React.js. I have recently picked up React so this was my first
-                  playing around project that uses a simple clock object and a input that creates an array
-                  of tasks. Each task has a checkbox associated with it which is kept track by a boolean
-                  value. </p>
-                  : null
-              }
+            <div id="left_col">
+                <li> WebApp To Do List </li>
+                  <img src={logo} alt="1st" onClick={() => this.onClick(0)}/>
+                  {
+                      this.state.children[0].childVisible
+                      ? <p> This app was build on React.js. I have recently picked up React so this was my first
+                      playing around project that uses a simple clock object and a input that creates an array
+                      of tasks. Each task has a checkbox associated with it which is kept track by a boolean
+                      value. </p>
+                      : null
+                  }
+                <li> Mobile Calculator</li>
+                <img src={calc} onClick={() => this.onClick(1)}/>
+                {
+                    this.state.children[1].childVisible
+                    ? <p> This iOS app is a simple Calculator app based on Swift 3 and built in xCode 9. I
+                    decided to start this project based on a need to create a calculator with arabic 
+                    numerals and brush up on my Swift programming skills. After compeleting
+                    all the functinallities for simple arithmetic operations to the 6th decimal point, I
+                    no longer felt the passion to continue the project. It 
+                    </p>
+                    : null
+                }
+                </div>
+            <div id = "right_col">
+             <li> Bookmark - Textbook Buy & Sell</li>
+                  <img src={bookmark} onClick={() => this.onClick(2)}/>
+                {
+                    this.state.children[2].childVisible
+                    ? <p> This iOS app was a result of a group project in a course at Simon Fraser Univerity
+                    in CMPT 275 - Intro to Software Engineering Techniques. We worked in this project in a 
+                    group of 4 and were able to create a solid product with a top notch design in a period
+                    of 4 months where 2 months was spent on ideation and documentation and 2 months on 
+                    implementation. 
+                
+                    This beauty was built on Firebase for the purpose creating user accounts and for keeping
+                    track of textbook inventory. We had to create a structure that would make it extremely 
+                    easy for the student to find the textbook for the course. So we decided to present the
+                    books by the levels of 
+                
+                    Department -> Course -> Course Number -> 3 Most Recent Semester's books
+                
+                    It was beautiful.
+                    </p>
+                    : null
+                }
+            </div>  
+        </ul>
             
-            
-            <li> Mobile Calculator</li>
-            <img src={logo} onClick={() => this.onClick(1)}/>
-            {
-                this.state.children[1].childVisible
-                ? <p> This iOS app is a simple Calculator app based on Swift 3 and built in xCode 9. I
-                decided to start this project based on a need to create a calculator with arabic 
-                numerals and brush up on my Swift programming skills. After compeleting
-                all the functinallities for simple arithmetic operations to the 6th decimal point, I
-                no longer felt the passion to continue the project. It 
-                </p>
-                : null
-            }
-            
-
-            <li> Bookmark - Textbook Buy & Sell</li>
-            <img src={logo} onClick={() => this.onClick(2)}/>
-            {
-                this.state.children[2].childVisible
-                ? <p> This iOS app was a result of a group project in a course at Simon Fraser Univerity
-                in CMPT 275 - Intro to Software Engineering Techniques. We worked in this project in a 
-                group of 4 and were able to create a solid product with a top notch design in a period
-                of 4 months where 2 months was spent on ideation and documentation and 2 months on 
-                implementation. 
-
-                This beauty was built on Firebase for the purpose creating user accounts and for keeping
-                track of textbook inventory. We had to create a structure that would make it extremely 
-                easy for the student to find the textbook for the course. So we decided to present the
-                books by the levels of 
-
-                Department -> Course -> Course Number -> 3 Most Recent Semester's books
-
-                It was beautiful.
-                </p>
-                : null
-            }
-            
-          </ul>
         </div>
       </div>
     );
